f8a93f407b76ff187b03f93dd1feaf1640e32dc4
qiaox606 <78217834+qiaox606@users.noreply.github.com>
2021-03-26 19:11:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -17,21 +17,9 @@ button.addEventListener('click', function (event) {
     counter.innerHTML = count;
 });
 
-container.addEventListener('click', function (event) {
-    console.log(event.target.tagName);
-    if (event.target.tagName == 'IMG'); {
-        event.target.remove();
-        var images = container.querySelectorAll('img');
-        var count = images.length;
-        console.log(count);
-        counter.innerHTML = count;
-    }
-});
-
 clear.addEventListener('click', function (clear) {
     container.innerHTML = clear;
     var images = container.querySelectorAll('img');
-        var count = images.length;
         container.remove()
 });
 
