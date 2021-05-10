37b80fe26ff3bf5802138b1c833c4fd30d7b4163
qiaox606 <78217834+qiaox606@users.noreply.github.com>
2021-05-11 02:35:06 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,98 @@
-
-var button = document.querySelector('#click');
+var button = document.querySelector('#clickSleep');
+var button2 = document.querySelector('#clickFeed');
+var button3 = document.querySelector('#clickGame');
 var container = document.querySelector('#container');
 var counter = document.querySelector('#counter')
 var clear = document.querySelector('#clear')
+var textCountId = document.querySelector('#textCountId');
 
+var box1Id = document.querySelector('#box1Id');
+var box2Id = document.querySelector('#box2Id');
+var box3Id = document.querySelector('#box3Id');
 
-button.addEventListener('click', function (event) {
-    var image = document.createElement('img');
-    image.src = 'footprint.png';
-    image.style.left = (100 * Math.random()) + '%';
-    image.style.top = (100 * Math.random()) + '%';
-    container.appendChild(image);
-    var images = container.querySelectorAll('img');
-    var count = images.length;
-    console.log(count);
-    counter.innerHTML = count;
+/* click to add sleep */
+button.addEventListener('click', function(event) {
+  let image = document.createElement('img');
+  image.classList.add('new-content');
+  image.src = 'https://cdn.glitch.com/82e3bc67-a491-459e-8fd4-76fc5df23096%2Fsleep.png?v=1620670086886';
+  image.style.right = (80 * Math.random()) + '%';
+  image.style.top = (90 * Math.random()) + '%';
+  container.appendChild(image);
+
+  var counter = document.querySelectorAll('.new-content');
+  var counterText = counter.length + ' ';
+  if (counter.length > 1) {
+    counterText += 'item(s)';
+  }
+  textCountId.innerHTML = counterText;
 });
 
-clear.addEventListener('click', function (clear) {
-    container.innerHTML = clear;
-    var images = container.querySelectorAll('img');
-        container.remove()
+
+/* click to add feed */
+button2.addEventListener('click', function(event) {
+  let image = document.createElement('img');
+  image.classList.add('new-content');
+  image.src = 'https://cdn.glitch.com/82e3bc67-a491-459e-8fd4-76fc5df23096%2Ffood.png?v=1620670088015';
+  image.style.right = (80 * Math.random()) + '%';
+  image.style.top = (90 * Math.random()) + '%';
+  container.appendChild(image);
+
+  var counter = document.querySelectorAll('.new-content');
+  var counterText = counter.length + ' ';
+  if (counter.length > 1) {
+    counterText += 'item(s)';
+  }
+  textCountId.innerHTML = counterText;
 });
 
-function changeText(id) {
-  id.innerHTML = "JavaScript DOM Manipulation";
-}
+/* click to add game */
+button3.addEventListener('click', function(event) {
+  let image = document.createElement('img');
+  image.classList.add('new-content');
+  image.src = 'https://cdn.glitch.com/82e3bc67-a491-459e-8fd4-76fc5df23096%2Fplay.png?v=1620670086817';
+  image.style.right = (80 * Math.random()) + '%';
+  image.style.top = (90 * Math.random()) + '%';
+  container.appendChild(image);
+
+  var counter = document.querySelectorAll('.new-content');
+  var counterText = counter.length + ' ';
+  if (counter.length > 1) {
+    counterText += 'item(s)';
+  }
+  textCountId.innerHTML = counterText;
+});
+
+/* remove */
+clear.addEventListener('click', function(event) {
+  var boxes = document.querySelectorAll('.new-content');
+  boxes.forEach(function(box) {
+    box.remove();
+  })
+});
+
+/* count the number */
+container.addEventListener('click', function(event) {
+  if (event.target.classList.contains('new-content') ) {
+    event.target.remove();
+  }
+
+  var counter = document.querySelectorAll('.new-content');
+  var counterText = counter.length + ' ';
+  if (counter.length > 1) {
+    counterText += 'recent number';
+  }
+  textCountId.innerHTML = counterText;
+});
+
+/* change background color */
+container.addEventListener('click', function(event) {
+  var target = event.target;
+  if (target.classList.contains('swatch')) {
+    var color = getComputedStyle(target)['backgroundColor'];
+    var boxes = document.querySelectorAll("BODY");
+
+    boxes.forEach(function(box) {
+      box.style.backgroundColor = color;
+    });
+  }
+});
